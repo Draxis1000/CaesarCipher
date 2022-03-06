29684a8aad4f63feb0fa380ce0f3341a0e11a89a
Draxis1000 <floresfranciskyle@yahoo.com>
2022-03-07 07:59:13 +0800

    Initial commit

---
+++ CaesarCipher/CaesarCipher/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CaesarCipher
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public static char Cipher(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+            {
+
+                return ch;
+            }
+
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - d) % 26) + d);
+
+
+        }
+
+        public static string Encipher(string input, int key)
+        {
+            string output = string.Empty;
+
+            foreach (char ch in input)
+                output += Cipher(ch, key);
+
+            return output;
+        }
+
+        public static string Decipher(string input, int key)
+        {
+            return Encipher(input, 26 - key);
+        }
+
+        private void btnEncrypt_Clicked(object sender, EventArgs e)
+        {
+            string UserString = txtPlainText.Text;
+
+            int key = Convert.ToInt32(txtKey.Text);
+
+            string cipherText = Encipher(UserString, key);
+
+            txtCipherText.Text = cipherText;
+        }
+    }
+}
